696f1179b881fff3f5024696c069db8688e01ee6
Raahi Menon <rm834@cornell.edu>
2020-05-09 12:58:03 -0500

    implemented hitbox json file

--- gameVars.ml
+++ gameVars.ml
@@ -20,3 +20,20 @@ let hud_bezel_tile = 0.5
 let hud_bezel_px = tile_size *. hud_bezel_tile |> int_of_float
 
 let inventory_size = vrad |> int_of_float
+
+let combat_objects = ["sword"]
+let buff_objects = ["blue-rupee"]
+
+let bounding_boxes = Yojson.Basic.from_file "boundbox.json"
+
+let boundbox_helper o value = 
+  Yojson.Basic.Util.member o bounding_boxes
+  |> Yojson.Basic.Util.member value
+  |> Yojson.Basic.Util.to_int
+
+let boundbox_width o = boundbox_helper o "width"
+let boundbox_height o = boundbox_helper o "height"
+let boundbox_wh o = (boundbox_width o, boundbox_height o)
+let boundbox_x o = boundbox_helper o "x"
+let boundbox_y o = boundbox_helper o "y"
+let boundbox_xy o = (boundbox_x o, boundbox_y o)
--- gameVars.mli
+++ gameVars.mli
@@ -19,3 +19,16 @@ val inventory_size : int
 
 val hud_bezel_tile : float
 val hud_bezel_px : int
+
+val combat_objects : string list
+val buff_objects : string list
+
+val bounding_boxes : Yojson.Basic.t
+
+val boundbox_width : string -> int
+val boundbox_height : string -> int
+val boundbox_wh : string -> int*int
+
+val boundbox_x : string -> int
+val boundbox_y : string -> int
+val boundbox_xy : string -> int*int

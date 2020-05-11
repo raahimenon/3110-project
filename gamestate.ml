126646120e227c0512e133789682800913df7692
Raahi Menon <rm834@cornell.edu>
2020-05-10 22:31:18 -0500

    adding some docs

--- gamestate.ml
+++ gamestate.ml
@@ -460,6 +460,10 @@ let room_updater st room =
    enemies = new_enemies;
    items = room.items |> List.filter_map (item_updater st) }
 
+(** [icons_of_int ?style num st] is the list of images representing [num] drawn
+    from icons with corresponding filenames in [st], according to [?style].
+    If style = 0 then the numbers will be loaded as white. If greater, green,
+    and if less, red. *)
 let icons_of_int ?style:(style = 0) num st : Animations.image list = 
   let num = string_of_int num in
   let rec icons accu = function
@@ -471,24 +475,19 @@ let icons_of_int ?style:(style = 0) num st : Animations.image list =
              (String.sub s 1 (String.length s - 1)) in
   icons [] num |> List.rev
 
-let icons_of_float ?style:(style = 0) num st : Animations.image list = 
-  if Float.floor num = num then icons_of_int ~style:style (int_of_float num) st 
-  else
-    let num = string_of_float num in
-    let rec icons accu = function
-      | str when str = "" -> accu
-      | s -> 
-        let fname = String.sub s 0 1 in
-        icons ((Animations.get_icon 
-                  ((if fname = "." then "dot" else fname )^ 
-                   if style > 0 then "p" else if style < 0 then "m" else "") 
-                  st.icons)::accu) 
-          (String.sub s 1 (String.length s - 1)) in
-    icons [] num |> List.rev
-
-let rec get_buffs items (accu) : float * int * int * int * int * int =
+(** [get_buffs buffs accu] evaluates to (catk,cspd,bh,bmh,batk,bspd), where
+    catk is the attack gained from the Combat stat of the item, 
+    cspd is the speed gained from the Combat stat of the item,
+    bh is the health gained from the Buff stat of the item,
+    bmh is the max health gained from the Buff stat of the item,
+    batk is the attack gained from the Buff stat of the item,
+    bspd is the speed gained from the Buff stat of the item.
+    Note that this function should only be called with buff items, so the 
+    first two elements of the 6-tuple will always be 1.,0 (attack is 1 as
+    it is a multiplier) *)
+let rec get_buffs buffs accu : float * int * int * int * int * int =
   let catk, cspd, bh, bmh, batk, bspd = accu in
-  match items with
+  match buffs with
   | [] -> accu
   | h::t -> begin match h with 
       | Buff.Health h -> get_buffs t (catk, cspd, h, bmh, batk, bspd)
@@ -497,142 +496,81 @@ let rec get_buffs items (accu) : float * int * int * int * int * int =
       | Buff.Movement_speed spd -> get_buffs t (catk, cspd, bh, bmh, batk, spd)
     end
 
-let draw_hud (st : state) = 
-  let player = st.current_room.player in
-  let ratio = float_of_int player.health /. (float_of_int player.max_health) in
-  let item = Room.get_item_slot st.current_room player.inventory_slot in
-  let combat_attack, combat_speed, buff_health, buff_mh, buff_atk, buff_spd
-    = match item with 
-    | Some i -> begin match i.unique_stats with 
-        |Combat {attack; movement_speed} -> attack, movement_speed, 0, 0, 0, 0
-        |Buff b -> get_buffs b.effect (0.,0,0,0,0,0) end
-    | None -> 0., 0, 0, 0, 0, 0 in
-  (* Initialize HUD box drawing *)
-  Window.draw_hud_box st.window st.current_room.player.inventory_slot;
-  (* Draw HUD health bar *)
+(** [draw_num_hud num win y] takes the image list of numbers [num] and draws
+    them to window [win] at y-position [y] at the assumed x-position in
+    the HUD*)
+let draw_num_hud num win y = 
+  List.iteri 
+    (fun idx ic -> 
+       Window.draw_image win ic 
+         (float_of_int width -. 2. +. 
+          (5./.tile_size)+. 
+          (float_of_int (idx + 3 - List.length num)) *. 
+          (9./.tile_size)) y) num
+
+(** [draw_health st player buff_health buff_mh ratio] draws the player's 
+    health stats to the HUD, as well as potential item effects*)
+let draw_health (st:state) player buff_health buff_mh ratio = 
+  let health_display = (icons_of_int (player.health) st) in
+  let max_health_display = icons_of_int (player.max_health) st in
   if(buff_health > 0) then begin
     let buff_health_display = icons_of_int ~style:1 (buff_health) st in
     Window.draw_image st.window (Animations.get_icon "+" st.icons)
-      (float_of_int GameVars.width -. 2.) 0.;
-    List.iteri 
-      (fun idx ic -> 
-         Window.draw_image st.window ic 
-           (float_of_int GameVars.width -. 2. +. 
-            (5./.GameVars.tile_size)+. 
-            (float_of_int (idx + 3 - List.length buff_health_display)) *. 
-            (9./.GameVars.tile_size))
-           (0.))
-      buff_health_display end
-  else ();
-  let health_display = (icons_of_int (player.health) st) in
-  List.iteri 
-    (fun idx ic -> 
-       Window.draw_image st.window ic 
-         (float_of_int GameVars.width -. 2. +. 
-          (5./.GameVars.tile_size)+. 
-          (float_of_int (idx + 3 - List.length health_display)) *. 
-          (9./.GameVars.tile_size))
-         (1.))
-    health_display;
-  Window.draw_rect_col st.window 
-    (Window.health_col_ratio ratio)
-    (float_of_int GameVars.width -. 1. -. 
-     GameVars.hud_bezel_tile,2.+.5./.GameVars.tile_size)
-    (1., GameVars.vrad*.ratio);
-  Window.draw_image_raw st.window
-    (Animations.get_icon "hp" st.icons)
-    (float_of_int GameVars.width -. 1. -. GameVars.hud_bezel_tile)
-    (2.);
-  List.iteri 
-    (fun idx ic -> 
-       Window.draw_image st.window ic 
-         (float_of_int GameVars.width -. 2. +. 
-          (2./.GameVars.tile_size)+. 
-          (float_of_int idx) *. 
-          (9./.GameVars.tile_size))
-         (2. +. 10./.GameVars.tile_size +. GameVars.vrad))
-    (icons_of_int (player.max_health) st);
+      (float_of_int width -. 2.) 0.;
+    draw_num_hud buff_health_display st.window 0. end;
+  draw_num_hud health_display st.window 1.;
+  Window.draw_rect_col st.window (Window.health_col_ratio ratio)
+    (float_of_int width -. 1. -. 
+     hud_bezel_tile,2.+.5./.tile_size)
+    (1., vrad*.ratio);
+  Window.draw_image_raw st.window (Animations.get_icon "hp" st.icons)
+    (float_of_int width -. 1. -. hud_bezel_tile) 2.;
+  draw_num_hud max_health_display st.window (2. +. 10./.tile_size +. vrad);
   if(buff_mh > 0) then begin
     let buff_mh_display = icons_of_int ~style:1 (buff_mh) st in
     Window.draw_image st.window (Animations.get_icon "+" st.icons)
-      (float_of_int GameVars.width -. 2.)
-      (3. +. 10./.GameVars.tile_size +. GameVars.vrad);
-    List.iteri 
-      (fun idx ic -> 
-         Window.draw_image st.window ic 
-           (float_of_int GameVars.width -. 2. +. 
-            (5./.GameVars.tile_size)+. 
-            (float_of_int (idx + 3 - List.length buff_mh_display)) *. 
-            (9./.GameVars.tile_size))
-           (3. +. 10./.GameVars.tile_size +. GameVars.vrad))
-      buff_mh_display end
-  else ();
-  (* Draw player stats *)
+      (float_of_int width -. 2.)
+      (3. +. 10./.tile_size +. vrad);
+    draw_num_hud buff_mh_display st.window (3. +. 10./.tile_size +. vrad) end
+
+(** [draw_attack st combat_attack buff_atk] draws the player's 
+    attack stats to the HUD, as well as potential item effects*)
+let draw_attack (st : state) combat_attack buff_atk = 
   Window.draw_image st.window
     (Animations.get_icon "attack" st.icons)
     (float_of_int GameVars.width -. 2.)
     (float_of_int GameVars.height -. 4.);
   let atk_display = icons_of_int 
       ~style:
-        (if combat_attack > 0. then 1 
-         else if combat_attack < 0. then -1 
-         else 0)
+        (if combat_attack > 1. then 1 else if combat_attack < 1. then -1 else 0)
       (player_attack st) st in
-  List.iteri 
-    (fun idx ic -> 
-       Window.draw_image st.window ic 
-         (float_of_int GameVars.width -. 2. +. 
-          (5./.GameVars.tile_size)+. 
-          (float_of_int (idx + 3 - List.length atk_display)) *. 
-          (9./.GameVars.tile_size))
-         (float_of_int GameVars.height -. 4.))
-    atk_display;
+  draw_num_hud atk_display st.window (float_of_int GameVars.height -. 4.);
   if(buff_atk > 0) then begin
     let buff_atk_display = icons_of_int ~style:1 (buff_atk) st in
     Window.draw_image st.window (Animations.get_icon "+" st.icons)
-      (float_of_int GameVars.width -. 2.)
-      (float_of_int GameVars.height -. 1.);
-    List.iteri 
-      (fun idx ic -> 
-         Window.draw_image st.window ic 
-           (float_of_int GameVars.width -. 2. +. 
-            (5./.GameVars.tile_size)+. 
-            (float_of_int (idx + 3 - List.length buff_atk_display)) *. 
-            (9./.GameVars.tile_size))
-           (float_of_int GameVars.height -. 3.))
-      buff_atk_display end;
+      (float_of_int GameVars.width -. 2.) (float_of_int GameVars.height -. 1.);
+    draw_num_hud buff_atk_display st.window (float_of_int GameVars.height -. 3.) end;
+  Window.draw_image st.window
+    (Animations.get_icon "speed" st.icons)
+    (float_of_int GameVars.width -. 2.)
+    (float_of_int GameVars.height -. 2.)
+
+(** [draw_attack st combat_attack buff_atk] draws the player's 
+    speed stats to the HUD, as well as potential item effects*)
+let draw_speed (st : state) combat_speed buff_spd = 
   Window.draw_image st.window
     (Animations.get_icon "speed" st.icons)
     (float_of_int GameVars.width -. 2.)
     (float_of_int GameVars.height -. 2.);
   let spd_display = icons_of_int ~style:
-      (if combat_speed > 0 then 1 
-       else if combat_speed < 0 then -1 
-       else 0)
+      (if combat_speed > 0 then 1 else if combat_speed < 0 then -1 else 0)
       (player_speed st) st in
-  List.iteri 
-    (fun idx ic -> 
-       Window.draw_image st.window ic 
-         (float_of_int GameVars.width -. 2. +. 
-          (5./.GameVars.tile_size)+. 
-          (float_of_int (idx + 3 - List.length spd_display)) *. 
-          (9./.GameVars.tile_size))
-         (float_of_int GameVars.height -. 2.))
-    spd_display;
+  draw_num_hud spd_display st.window (float_of_int GameVars.height -. 2.);
   if(buff_spd > 0) then begin
     let buff_spd_display = icons_of_int ~style:1 (buff_spd) st in
     Window.draw_image st.window (Animations.get_icon "+" st.icons)
-      (float_of_int GameVars.width -. 2.)
-      (float_of_int GameVars.height -. 1.);
-    List.iteri 
-      (fun idx ic -> 
-         Window.draw_image st.window ic 
-           (float_of_int GameVars.width -. 2. +. 
-            (5./.GameVars.tile_size)+. 
-            (float_of_int (idx + 3 - List.length buff_spd_display)) *. 
-            (9./.GameVars.tile_size))
-           (float_of_int GameVars.height -. 1.))
-      buff_spd_display end;
+      (float_of_int GameVars.width -. 2.) (float_of_int GameVars.height -. 1.);
+    draw_num_hud buff_spd_display st.window (float_of_int GameVars.height -. 1.) end;
   (* Draw inventory *)
   List.map (fun (i : Item.t) ->
       match i.pos with 
@@ -641,6 +579,26 @@ let draw_hud (st : state) =
           st.current_room.player.e.pos i 
       |_ -> ()) st.current_room.items
 
+(** [draw_hud st] draws the HUD of state [st] to its window*)
+let draw_hud (st : state) = 
+  let player = st.current_room.player in
+  let ratio = float_of_int player.health /. (float_of_int player.max_health) in
+  let item = Room.get_item_slot st.current_room player.inventory_slot in
+  let combat_attack, combat_speed, buff_health, buff_mh, buff_atk, buff_spd
+    = match item with 
+    | Some i -> begin match i.unique_stats with 
+        |Combat {attack; movement_speed} -> attack, movement_speed, 0, 0, 0, 0
+        |Buff b -> get_buffs b.effect (1.,0,0,0,0,0) end
+    | None -> 1., 0, 0, 0, 0, 0 in
+  (* Draw HUD *)
+  Window.draw_hud_box st.window st.current_room.player.inventory_slot;
+  draw_health st player buff_health buff_mh ratio;
+  draw_attack st combat_attack buff_atk;
+  draw_speed st combat_speed buff_spd
+
+(** [gen_room_check st] is the truth value of whether or not a new room should 
+    be created. This is true if a player is interacting with a [Room.Exit] and
+    false otherwise.*)
 let gen_room_check st =
   let player = st.current_room.player in
   match player.state with
@@ -651,48 +609,63 @@ let gen_room_check st =
     List.length exit > 0
   | _ -> false
 
+(** [draw_room st] draws the current room and HUD of [st] to its window*)
+let draw_room st = 
+  Window.clear st.window;
+  Room.draw_room st.window st.current_room; 
+  ignore (draw_hud st);
+  Window.render st.window 
+
+(** [load_room st] is a [Room.t] of the next room created after the 
+    current one in [st]*)
+let load_room st =
+  let () = Random.init (st.current_room.seed) in
+  let rm_ref : Room.t option ref = ref None in
+  Thread.create (fun seed ->
+      rm_ref := Some (Room_gen.simple_gen (Random.int 1073741823) st.window)) 
+    () |> ignore;
+  let frame = ref 0 in
+  let loading = Animations.load_animation "./sprites/loading/" "loading" 
+      (Window.get_renderer st.window) in
+  let time = Window.get_time () |> ref in
+  while (!rm_ref = None) do 
+    Window.clear st.window;
+    Window.draw_image st.window (Animations.frame loading !frame) (hrad -. 2.) (vrad -. 2.);
+    Window.render st.window;
+    if Window.get_time () - !time > GameVars.anim_spf_in_milli then begin
+      time := Window.get_time ();
+      frame := (!frame + 1) mod (snd loading |> Array.length) end;
+  done;
+  Option.get !rm_ref
+
+(** [create_new_room st] is a [Room.t] of the next room created after the 
+    current one in [st] updated with all the elements of [st.current_room]
+    that should carry over *)
+let create_new_room st =
+  let rm = load_room st in
+  let pos = rm.player.e.pos in
+  {rm with player = 
+             {st.current_room.player with 
+              paused = false; 
+              e = {st.current_room.player.e with 
+                   pos = pos; 
+                   curr_tile = to_int pos};};
+           items = Room.get_inventory st.current_room @ rm.items}
+
+(** [game_loop st time] is the main game loop. It draws the current room 
+    to the screen, checks for input, and updates based on the input *)
 let rec game_loop st time = 
   let curr_time = Window.get_time () in
   let delta = curr_time - time in 
   let delay = spf_in_milli - delta in
-
   begin if (delay) > 0 then Window.wait (delay) 
     else print_endline ("lag" ^ string_of_int delay)
   end;
-
   let curr_time = Window.get_time () in
-
-  Window.clear st.window;
-  Room.draw_room st.window st.current_room; 
-  ignore (draw_hud st);
-  Window.render st.window;
-
+  draw_room st;
   let next_room = 
     if gen_room_check st 
-    then 
-      let () = Random.init (st.current_room.seed) in
-      let rm_ref : Room.t option ref = ref None in
-      Thread.create (fun seed -> rm_ref := Some (Room_gen.simple_gen (Random.int 1073741823) st.window)) () |> ignore;
-      let frame = ref 0 in
-      let loading = Animations.load_animation "./sprites/loading/" "loading" (Window.get_renderer st.window) in
-      let time = Window.get_time () |> ref in
-      while (!rm_ref = None) do 
-        Window.clear st.window;
-        Window.draw_image st.window (Animations.frame loading !frame) (hrad -. 2.) (vrad -. 2.);
-        Window.render st.window;
-        if Window.get_time () - !time > GameVars.anim_spf_in_milli then begin
-          time := Window.get_time ();
-          frame := (!frame + 1) mod (snd loading |> Array.length) end;
-      done;
-      let rm = !rm_ref |> Option.get in
-      let pos = rm.player.e.pos in
-      {rm with player = 
-                 {st.current_room.player with 
-                  paused = false; 
-                  e = {st.current_room.player.e with 
-                       pos = pos; 
-                       curr_tile = to_int pos};};
-               items = Room.get_inventory st.current_room @ rm.items}
+    then create_new_room st
     else room_updater st st.current_room in
 
   let input = Window.input_query st.input in
